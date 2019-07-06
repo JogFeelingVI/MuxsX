431dfae2063aa38cb0c113a7debbd9d5ca86952c
JogFeelingVI <lifelse@outlook.com>
2019-07-06 17:31:05 +0800

    Add New Py

--- MuxsX.py
+++ MuxsX.py
@@ -5,15 +5,20 @@
 # @Link	: blog.sina.com.cn/lifelse
 # @Name	: MuxsX
 import argparse
+import enum
 
-class vars:
+class vars(enum.Enum):
     d = list('0123456789')
     SPC = list('._-')
+    Split =','
 
 class argsx:
     """ 参数设定 """
     def __init__(self):
-        parg = argparse.ArgumentParser()
+        parg = argparse.ArgumentParser(prog='MuxsX.py', description='MuxsX by FeelingVi 1.2', usage='lifelse')
+        parg.add_argument('-d', dest='del_str', help='del string', type=str)
+        parg.add_argument('-a', dest='add_str', help='add string', type=str)
+        parg.add_argument('-r', dest='rex_str', help='rex string', type=str)
 
 def rLoads() -> str:
     return '0xNes'
--- README.md
+++ README.md
@@ -6,3 +6,4 @@
  · Building, please stay tuned
  · 건물, 계속 지켜봐주십시오.
  · 建物は、ご期待ください
+

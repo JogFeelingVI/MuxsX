ebb7b7385772aa56c89bb9f53c2d146e886aab8a
JogFeelingVI <lifelse@outlook.com>
2019-07-06 16:59:26 +0800

    Add New Py

---
+++ MuxsX.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date	: 2019-07-06 15:02
+# @Author  : Lifelse (Lifelse@outlook.com)
+# @Link	: blog.sina.com.cn/lifelse
+# @Name	: MuxsX
+import argparse
+
+class vars:
+    d = list('0123456789')
+    SPC = list('._-')
+
+class argsx:
+    """ 参数设定 """
+    def __init__(self):
+        parg = argparse.ArgumentParser()
+
+def rLoads() -> str:
+    return '0xNes'
+
+if __name__ == '__main__':
+    rLoads()
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # MuxsX
- MuxsX for windows linux macos
+#### MuxsX for windows linux macos
+
+> It is used to process large files, rename, sort, and number. You can build scripts for subsequent file processing. But this is only in the concept, the code is still in the test phase!
+
+ · Building, please stay tuned
+ · 건물, 계속 지켜봐주십시오.
+ · 建物は、ご期待ください

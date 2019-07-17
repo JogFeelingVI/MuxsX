3432fa9942c3ac5f85f2eccd7d813d83885a9afc
JogFeelingVI <lifelse@outlook.com>
2019-07-17 16:02:28 +0800

    Add New Py

--- MuxsX.py
+++ MuxsX.py
@@ -6,10 +6,11 @@
 # @Name	: MuxsX
 import argparse
 import enum
-import glob
+import pathlib
 
 class vars(enum.Enum):
     d = list('0123456789')
+    def_type = '*.jpg' # 任意文件 但不包含文件夹
     SPC = list('._-')
     Split =','
     NoPath = 'No Path'
@@ -18,12 +19,8 @@ class vars(enum.Enum):
     h_pat = 'Specify the file path, which can be a folder or file name'
     h_add = 'Add string to file name'
     h_rex = 'Replace the string specified in the file name'
+    h_typ = 'Specify file type'
 
-class globsFb:
-    """ list all file from path """
-    @staticmethod
-    def list_all_files(fx:str = '*.*', path:str = vars.NoPath) -> dict:
-        return {'fx': fx, 'file':glob.glob(path)}
 
 class argsx:
     """ 参数设定 """
@@ -31,19 +28,31 @@ class argsx:
     __Args = None
     def __init__(self):
         parg = argparse.ArgumentParser(prog='MuxsX.py', description='MuxsX by FeelingVi 1.2', usage='lifelse')
-        parg.add_argument('-d', help=vars.h_del.value, type=str)
-        parg.add_argument('-a', help=vars.h_add.value, type=str)
-        parg.add_argument('-r', help=vars.h_rex.value, type=str)
-        parg.add_argument('path', help=vars.h_pat.value, type=str, nargs='*')
+        parg.add_argument('-d', dest='del', help=vars.h_del.value, type=str)
+        parg.add_argument('-a', dest='add', help=vars.h_add.value, type=str)
+        parg.add_argument('-r', dest='replace', help=vars.h_rex.value, type=str)
+        parg.add_argument('-type', dest='type', help=vars.h_typ.value, type=str, default=vars.def_type.value)
+
+        parg.add_argument(dest='path', help=vars.h_pat.value, type=str, default='~/Downloads')
         self.__Args = parg.parse_args()
 
+    def __glob_file(self, type:str = vars.def_type.value) -> list:
+        FxPth = self.__FixAgs.get('path')
+        Path = pathlib.Path(FxPth)
+        files = list(Path.glob(type))
+        print('{:.<20}: {}'.format('[E]File', files[0]))
+        return files
 
     def load_args(self):
         for k, v in self.__Args.__dict__.items():
-            print('{:.<20}: {}'.format('[A]' + k, v))
             if v is not None:
                 self.__FixAgs[k] = v
-        print(self.__FixAgs)
+                print('{:.<20}: {}'.format('[A]' + k, v))
+        #开始处理需要处理的参数
+        #path
+        self.files = self.__glob_file(self.__FixAgs.get('type'))
+        print('{:.<20}: {}'.format('[F]File', self.files.__len__()))
+
 
 
 def run_pro():

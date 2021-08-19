d2204d9dbf02504e8a99c1e88a85779f89de2ce7
JogfeeingVI <lifelse@outlook.com>
2021-08-19 20:42:29 +0800

    0842

--- modex/core.py
+++ modex/core.py
@@ -10,24 +10,36 @@ class call:
     __Fix_args = {'var': 1.02}
     files = None
 
+    @classmethod
+    def fix_path(cls, key, VALE):
+        if (fix_item := osp.pathx(VALE)) is not None:
+            cls.files = osp.find(fix_item)
+            cls.__Fix_args[key] = fix_item
+        else:
+            cls.__Fix_args[key] = None
+            print(f'Error: Path {fix_item} Directory does not exist')
+
+    @classmethod
+    def fix_type(cls, key, VALE):
+        if ',' in VALE:
+            cls.__Fix_args[key] = [f'.{x}' for x in VALE.split(',')]
+        else:
+            cls.__Fix_args[key] = [f'.{VALE}']
+
+    @classmethod
+    def fix_ohes(cls, key, VALE):
+        cls.__Fix_args[key] = VALE
+
     def __init__(self, args: dict = None):
         if (AT := type(args)) != dict:
             print(f'Error: Parameters must be of type dict, args-T is {AT}')
             return
+        fix = {
+            'path': lambda k, v: self.fix_path(k, v),
+            'type': lambda k, v: self.fix_type(k, v),
+            'OxOO': lambda k, v: self.fix_ohes(k, v),
+        }
         for k, v in args.items():
             if v is not None:
-                if k == 'path':
-                    if (fix_v := osp.pathx(v)) is not None:
-                        self.__Fix_args[k] = fix_v
-                        self.files = osp.find(fix_v)
-                    else:
-                        self.__Fix_args[k] = None
-                        print(f'Error: Path {v} Directory does not exist')
-                elif k == 'type':
-                    if ',' in v:
-                        self.__Fix_args[k] = [f'.{x}' for x in v.split(',')]
-                    else:
-                        self.__Fix_args[k] = [f'.{v}']
-                elif True:
-                    self.__Fix_args[k] = v
+                fix[k](k, v) if k in fix.keys() else fix['OxOO'](k, v)
         print(self.__Fix_args)
